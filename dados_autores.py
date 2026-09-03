d99237a888f31195a690b30f3c627044097caf75
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-03 14:59:03 -0300

    coisei autores

---
+++ dados_autores.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+#conectando o banco de dados. Caso não exista, o banco é criado.
+conn = sqlite3.connect("biblioteca.db")
+
+#apaga a tabela autores
+conn.execute("DROP TABLE IF EXISTS autores")
+
+#cria a tabela autores
+conn.execute("CREATE TABLE autores (id INTEGER PRIMARY KEY AUTOINCREMENT \
+             , nome TEXT NOT NULL)")
+
+#inserindo os registros na tabela autores
+conn.executemany("INSERT INTO autores(nome) VALUES(?)",
+                 [("Kamome Shirahama",), ("Minhau",)])
+
+#confirmando a criação e os inserts da tabela autores.
+conn.commit()
+
